861b80fd0ce04563000dd172b599323db55d0344
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2025-10-07 00:32:52 +0530

    Make the button move away when hovered for extra uselessness

--- js/script.js
+++ js/script.js
@@ -16,3 +16,15 @@ button.addEventListener('click', () => {
     button.style.transform = 'scale(1)';
   }, 100);
 });
+
+
+// Button dodge on hover
+button.addEventListener('mouseover', () => {
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+  button.style.position = 'absolute';
+  button.style.left = `${randomX}px`;
+  button.style.top = `${randomY}px`;
+});
